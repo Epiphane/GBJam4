cf200ac8b0ee373a0591a586050d268f51720fd6
mlinsenbard <m.linsenbard@gmail.com>
2015-08-10 21:04:17 -0700

    added enemy and advPhysics component shells. im bad at this im so sorry

--- js/component/enemy.js
+++ js/component/enemy.js
@@ -1,43 +1,30 @@
-var sfx = new Juicy.SFX();
-sfx.load('goal', 'audio/fx_jump.mp3');
-
-Juicy.Component.create('Player', {
+Juicy.Component.create('Enemy', {
     constructor: function(myEntity) {
         this.speed = 200;
-    
-        this.controls = ['LEFT', 'RIGHT', 'DOWN', 'UP'];
-
-        /** Lets us keep track of what spritesheet direction we're using */
         this.direction = 'IDLE';
 
-        this.arrow = document.createElement('canvas');
-        this.arrow_context = this.arrow.getContext('2d');
-
-        this.lives = 3;
         this.health = 100;
-        this.baseDmg = 20;
-    },
+        this.baseDmg = 10;
 
-    score: function() {
-        this.entity.state.score();
-    },
+        // Where the enemy can take damage from
+        // Not sure how to integrate, but options will be
+        // 'ALL', 'TOP', 'BOTTOM', 'LEFT', 'RIGHT', 'PROJECTILE'
+        this.weakPoint = 'ALL';
 
-    loseLife: function() {
-        this.lives -= 1;
-        // Call death animation + sound?
+        // Same as above
+        // 'AGGRESSIVE_SURFACE' - Towards player on surface
+        // 'AGGRESSIVE_FLY' - Towards player; can move freely
+        // 'AGGRESSIVE_DIG' - Towards player while digging
+        // 'IDLE_SURFACE' - Moves back and forth on surface
+        // 'IDLE_FLY' - Moves randomly; can move freely
+        // 'IDLE_DIG' - Digs around randomly
+        // 'NONE' - completely stationary
+        this.movePattern = 'NONE';
 
-        if (this.lives == 0) {
-            this.RIP();
-        }
-    },
-
-    RIP: function() {
-        // probably call some game.setState(gameover) or something
-        // idfk i have no idea what im doing
     },
 
     startIdleAnim: function() {
-        this.entity.getComponent('ColoredSprite').runAnimation(8, 19, 0.16, true);
+        //this.entity.getComponent('ColoredSprite').runAnimation(8, 19, 0.16, true);
     },
 
     updateAnim: function(newDirection) {
@@ -68,8 +55,12 @@ Juicy.Component.create('Player', {
         }
     },
 
+    die: function() {
+        // TODO: WRITE ME!!!
+        console.log('oh noes, da enemi is ded');
+    },
+
     update: function(dt, game) {
-        var digger = this.entity.getComponent('Digger');
         var newDirection = 'IDLE';
 
         var self = this;
@@ -115,72 +106,31 @@ Juicy.Component.create('Player', {
 
         this.updateAnim(newDirection);
 
-        if (this.entity.state.target.testCollision(this.entity)) {
-            this.score();
-            sfx.play('goal');
+        if (game.getPlayer().entity.testCollision(this.entity)) {
+            // In here, test further for this.weakPoint
+            var player =  game.getPlayer();
+            if (this.weakPoint == 'ALL') {
+                this.health -= player.baseDmg * Math.abs(playerself.entity.getComponent('Physics').dy/70);
+            }
+        }
+
+        if (this.health <= 0) {
+            this.die(); // TODO: WRITE ME!!!
         }
     },
     render: function(context) {
-        var target = this.entity.state.target;
-
         var entity_center = this.entity.center();
-        var distanceToTarget = entity_center.sub(target.center());
-        var angleToTarget = Math.atan2(distanceToTarget.y, distanceToTarget.x) - Math.PI / 2;
-
-        var arrow_length = Math.min(40, distanceToTarget.length() / 2);
-        var arrow_width  = Math.floor(arrow_length / 25);
-
         this.arrow.width = 2 * (arrow_length * Math.abs(Math.sin(angleToTarget)) + arrow_width *  Math.abs(Math.cos(angleToTarget))) + 10;
         this.arrow.height = 2 * (arrow_width * Math.abs(Math.sin(angleToTarget)) + arrow_length *  Math.abs(Math.cos(angleToTarget))) + 10;
-
-        var arrowData = context.createImageData(this.arrow.width, this.arrow.height);
-        var data = arrowData.data;
-
-        function setPixel(point, color) {
-            if (point.x < 0 || point.x >= self.arrow.width || point.y < 0 || point.y >= self.arrow.height)
-                return;
-
-            var i = 4 * (point.x + point.y * self.arrow.width);
-            data[i+0]=color[0];
-            data[i+1]=color[1];
-            data[i+2]=color[2];
-            data[i+3]=color[3];
-        }
 
         var self = this;
         var center = new Juicy.Point(this.arrow.width / 2, this.arrow.height / 2);
         var step = distanceToTarget.mult(-1 / distanceToTarget.length());
-        function castPixels(position, color) {
-            var pos = position;
-            while (position.sub(pos).length() < arrow_length) {
-                var p = pos.floor();
-
-                // Gotta be far away from center
-                if (center.sub(p).length() > 10) {
-                    setPixel(p, color);
-                }
-
-                pos = pos.add(step);
-            }
-
-            // We're at the end of the arrow. Draw lines back to make the L shape
-            var horiz = step.rotate(Math.PI * 3 / 4);
-            var vert  = step.rotate(-Math.PI * 3 / 4);
-
-            for (var dist = 0; dist < 10; dist ++) {
-                setPixel(pos.add(horiz.mult(dist)).floor(), color);
-                setPixel(pos.add(vert .mult(dist)).floor(), color);
-            }
-        }
 
         for (var i = -arrow_width; i <= arrow_width; i ++) {
             for (var j = -arrow_width; j <= arrow_width; j ++) {
                 castPixels(center.add(i, j), Palette.get('MID'));
             }
         }
-
-        this.arrow_context.putImageData(arrowData, 0, 0);  
-
-        context.drawImage(this.arrow, (this.entity.width - this.arrow.width) / 2, (this.entity.height - this.arrow.height) / 2);
     }
 });
