fea81c16f976265d47b2f255225269694c6d38ba
Thomas Steinke <thomas.steinke@weebly.com>
2015-08-13 13:47:05 -0700

    Made sure particles didn't leak and plugged em back in

--- js/component/particles.js
+++ js/component/particles.js
@@ -1,13 +1,11 @@
 Juicy.Component.create('ParticleManager', {
     constructor: function() {
-        return;
         this.pendingParticles = Array();
         this.particles = Array();
     },
 
     /* Required: color, size, howMany, timeToLive, initThisParticle, updateParticle */
     spawnParticles: function(config) {
-        return;
         this.howMany = config.howMany;
         this.updateFunction = config.updateParticle;
 
@@ -25,13 +23,11 @@ Juicy.Component.create('ParticleManager', {
     },
 
     initParticle: function(currParticle) {
-        return;
         this.particles.push(currParticle);
         currParticle.init(currParticle);
     },
 
     update: function(dt, input) {
-        return;
 
         /**
          * Go through the particles that haven't spawned yet and check
@@ -62,7 +58,6 @@ Juicy.Component.create('ParticleManager', {
     },
 
     render: function(context) {
-        return;
         context.mozImageSmoothingEnabled = false;
         context.imageSmoothingEnabled = false;
         for (var i = 0; i < this.particles.length; i++) {
